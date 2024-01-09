49bb0420a4e334f84e43623332eaced493669c0f
Thiagouvea <thiagouvea90@gmail.com>
2024-01-09 11:56:05 -0300

    carroussel concluido

--- src/Componentes/Banner/index.js
+++ src/Componentes/Banner/index.js
@@ -1,19 +1,53 @@
-import styles from './Banner.module.css'
-// criar banner rotativo
+import { useEffect, useState, useRef } from 'react';
+import styles from './Banner.module.css';
 
-const Banner = () => {
-    return (
-        <div className={styles.carroussel}>
-            <div className={styles.item}>
-                <div className={styles.imagem}>
-                    <img src='assets/img/Banner1.png' alt='produto' />
-                </div>
-                <div className={styles.imagem}>
-                    <img src='assets/img/Banner2.png' alt='produto' />
-                </div>
+function Banner() {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(() => {
+    fetch('assets/shoes.json')
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  if (!data || !data.length) return null;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.carousel} ref={carousel}>
+        {data.map((item) => {
+          const { id, name, image } = item;
+          return (
+            <div className={styles.item} key={id}>
+              <div className={styles.image}>
+                <img src={image} alt={name} />
+              </div>
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+      <div className={styles.buttons}>
+        <button onClick={handleLeftClick}>
+          <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Left" />
+        </button>
+        <button onClick={handleRightClick}>
+          <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Right" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
